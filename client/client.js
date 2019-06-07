eb91afa9739909b709f2b83d39047601c3db6b9c
Edward Gao <egao@sent.com>
2019-06-06 23:46:09 -0700

    pi client done

--- client/client.js
+++ client/client.js
@@ -1,7 +1,12 @@
-const io = require("socket.io-client");
-const socket = io("https://edwardgao.com");
-const Gpio = require("onoff").Gpio;
-const exec = require("child_process").exec;
+var io = require("socket.io-client");
+var socket = io("https://edwardgao.com");
+var Gpio = require("onoff").Gpio;
+var exec = require("child_process").exec;
+
+var redLED = new Gpio(27, "out");
+var blueLED = new Gpio(23, "out");
+
+var cops = {"red": null, "blue": null}
 
 socket.on("connect", _ => {
     console.log("Socket ID:", socket.id);
@@ -30,4 +35,40 @@ socket.on("reqAuth", data => {
             }
         }
     });
+});
+
+socket.on("control", type => {
+    console.log("Command:", type);
+    function stopCops() {
+        if(!!cops.red) {
+            clearInterval(cops.red);
+            cops.red = null;
+        }
+        if(!!cops.blue) {
+            clearInterval(cops.blue);
+            cops.blue = null;
+        }
+    }
+    if(type == "open door") {
+        var doorlock = new Gpio(17, "out");
+        doorlock.writeSync(1);
+        setTimeout(_ => doorlock.writeSync(0), 1000);
+        setTimeout(_ => doorlock.unexport(), 1200);
+    } else if(type == "red pill") {
+        stopCops();
+        redLED.writeSync(redLED.readSync() ^ 1);
+    } else if(type == "blue pill") {
+        stopCops();
+        blueLED.writeSync(blueLED.readSync() ^ 1);
+    } else if(type == "shut down") {
+        stopCops();
+        redLED.writeSync(0);
+        blueLED.writeSync(0);
+    } else if(type == "wee woo") {
+        stopCops();
+        redLED.writeSync(1);
+        blueLED.writeSync(0);
+        cops.red = setInterval(_ => redLED.writeSync(redLED.readSync() ^ 1), 100);
+        cops.blue = setInterval(_ => blueLED.writeSync(blueLED.readSync() ^ 1), 100);
+    }
 });
