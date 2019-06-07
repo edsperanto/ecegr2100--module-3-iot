86374a23fa751d40419d7d5f35bb9bfdf3d24b9e
Edward Gao <egao@sent.com>
2019-06-07 05:21:09 +0000

    auth chain server side

--- server/server.js
+++ server/server.js
@@ -2,20 +2,49 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {};
+var piUser = "";
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
 
-    console.log('a user connected');
+    console.log('A user connected:', socket.id);
 
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
+    socket.on("identity", function(msg) {
+        console.log("This is a", msg);
+        if(msg == "pi") {
+            piUser = socket.id;
+        }
+        console.log("piUser:", piUser);
     });
 
-    socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
+    socket.on("register", function(data) {
+        [name, email, passwd, ssn] = data.split(';');
+        users[email] = {"name": name, "passwd": passwd, "ssn": ssn};
+        console.log("Registering:", email);
+    });
+
+    socket.on("cliAuth", function(data) {
+        [email, serial] = data.split(';');
+        users[email]["serial"] = serial;
+        console.log("Auth check:", serial);
+        io.to(piUser).emit("reqAuth", `${email};${serial}`);
+    });
+
+    socket.on("piAuth", function(data) {
+        [email, status] = data.split(';');
+        if(status == "success") {
+            users[email]["pirate"] = "no";
+        } else {
+            users[email]["pirate"] = "yes";
+        }
+    });
+
+    socket.on('disconnect', function() {
+        console.log('A user disconnected:', socket.id);
     });
 
 });
